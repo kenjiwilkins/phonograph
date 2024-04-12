2156f5ae59504e25583b77b187c3cb76916dc9e0
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2024-04-13 00:13:41 +0900

    Add: Unit Test - PlaylistList

--- src/components/PlaylistList.vue
+++ src/components/PlaylistList.vue
@@ -1,10 +1,12 @@
 <template>
   <ul v-if="hasSavedPlaylists" class="flex w-full flex-col gap-2 py-2">
     <li
-      v-for="playlist in savedPlaylists"
+      v-for="(playlist, index) in savedPlaylists"
       :key="playlist.id"
       class="flex w-full gap-2 px-2"
       @click="selectPlaylist(playlist)"
+      role="button"
+      :data-testid="`playlist-li-${index}`"
     >
       <content-image
         :src="playlist.images ? playlist.images[0].url : playlistIcon"
@@ -13,8 +15,16 @@
         :width="64"
       />
       <div class="flex w-full max-w-full flex-col justify-center overflow-x-hidden text-white">
-        <p class="overflow-x-hidden text-ellipsis whitespace-nowrap">{{ playlist.name }}</p>
-        <p class="text-md overflow-x-hidden text-ellipsis whitespace-nowrap font-thin">
+        <p
+          class="overflow-x-hidden text-ellipsis whitespace-nowrap"
+          :data-testid="`playlist-li-playlist-name-${index}`"
+        >
+          {{ playlist.name }}
+        </p>
+        <p
+          class="text-md overflow-x-hidden text-ellipsis whitespace-nowrap font-thin"
+          :data-testid="`playlist-li-playlist-total-${index}`"
+        >
           {{ playlist.tracks.total }} tracks
         </p>
       </div>
---
+++ src/tests/components/PlaylistList.spec.ts
@@ -0,0 +1,54 @@
+import { render, fireEvent } from '@testing-library/vue';
+import { createTestingPinia } from '@pinia/testing';
+import { describe, test, expect, vi } from 'vitest';
+import PlaylistList from '@/components/PlaylistList.vue';
+import { generatePlaylists } from '../helpers';
+import { useUserSavedPlaylistsStore } from '@/data';
+
+describe('PlaylistList', () => {
+  test('renders correctly - is loading', () => {
+    const playlists = generatePlaylists(10);
+    const { getByTestId } = render(PlaylistList, {
+      global: {
+        plugins: [
+          createTestingPinia({
+            initialState: {
+              userSavedPlaylists: {
+                playlists
+              }
+            }
+          })
+        ]
+      }
+    });
+    for (let i = 0; i < playlists.length; i++) {
+      const playlistName = getByTestId(`playlist-li-playlist-name-${i}`);
+      expect(playlistName).toBeTruthy();
+      expect(playlistName.textContent).toContain(playlists[i].name);
+      const playlistTrackCount = getByTestId(`playlist-li-playlist-total-${i}`);
+      expect(playlistTrackCount).toBeTruthy();
+      expect(playlistTrackCount.textContent).toContain(playlists[i].tracks.total);
+    }
+  });
+  test('functionality - click', async () => {
+    const playlists = generatePlaylists(10);
+    const { getByTestId } = render(PlaylistList, {
+      global: {
+        plugins: [
+          createTestingPinia({
+            initialState: {
+              userSavedPlaylists: {
+                playlists
+              }
+            },
+            stubActions: false
+          })
+        ]
+      }
+    });
+    const playlistStore = useUserSavedPlaylistsStore();
+    const playlistItem = getByTestId('playlist-li-0');
+    await fireEvent.click(playlistItem);
+    expect(playlistStore.selectedPlaylist).toEqual(playlists[0]);
+  });
+});
